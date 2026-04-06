b3a6bfa706645e89ded469e5226f68ed6e8fafe8
Bel1301 <belenpena009@gmail.com>
2026-04-06 15:35:51 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'nasadaily-v1';
+const STATIC = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
